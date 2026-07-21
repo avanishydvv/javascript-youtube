c3571c4cec39c1eddc5baacee2e097f6d525d2bf
Avanish Yadav <avanishydv29@gmail.com>
2026-07-21 23:30:25 +0530

    Auto

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14451
+let accountEmail = "avn@gmail.com"
+var accoutpassword = "12345"
+accountcity = "Gorakhpur" 
+
+
+//accountId = 2 // not allowed
+
+accountEmail = "asd@gmail.com"
+accoutpassword = "1274"
+accountcity = "kota"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accoutpassword,accountcity])
