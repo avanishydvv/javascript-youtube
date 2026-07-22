ac1a7df3967083cc5c0dce760f06bfbbc6b3cadc
Avanish Yadav <avanishydv29@gmail.com>
2026-07-22 21:42:57 +0530

    Auto

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //Treat all JS code as newer version //
+
+//alert("Hello") // we are using node js not browser //
+
+console.log(3+3);
+
+let name ="Avanish"
+let age = 19
+let isLoggedIn = false
+let state;
+
+/* number=> 2 to power 53
+    bigint
+    string=""
+
+    boolean=>true/False
+    null => stnadalone value
+    undefined => variable is declared but value is not assigned
+    symbol =
+*/
