24c14bfdc5aaf156ba54052a66aabb3c6ecf70b9
Avanish Yadav <avanishydv29@gmail.com>
2026-07-23 06:53:48 +0530

    Auto

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,12 +1,12 @@
-let score = "Avanish"
+// let score = "Avanish"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc"=> NaN
@@ -22,8 +22,50 @@ console.log(valueInNumber);
 // " => false ;
 // "avanish" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************** Operations ***************************** //
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%5);
+
+
+let str1 = "Avanish"
+let str2 = " hello"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1 , num2 , num3 
+
+num1 = num2 =num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
