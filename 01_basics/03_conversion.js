a46b55a87d7352cdc1687c028cd308766443f609
Avanish Yadav <avanishydv29@gmail.com>
2026-07-23 22:32:58 +0530

    Auto

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -69,3 +69,4 @@ let gameCounter = 100
 gameCounter++;
 console.log(gameCounter);
 
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+//lec -8
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2 == 1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === (Strict check data type also)
+
+console.log("2" === 2);
