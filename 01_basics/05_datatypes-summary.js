9227cf0a3419ffd8cb4362e1f577bffd296fad00
Avanish Yadav <avanishydv29@gmail.com>
2026-07-23 22:53:23 +0530

    Auto

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive Data types
+
+// 7 types : String,Number,Boolean,Null,undefined,Symbol,
+// BigInt 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('321')
+const anotherId = symbol('321')
+
+console.log(id === anotherId);
+
+const bigNumber = 36756368368768558n
+
+
+
+
+
+//Reference(Non-Primitive)
+
+// Array,Objects,Functions
+
+const heros = ["shaktiman","doga","b=naagraj"];
+{
+    name:"avanish";
+    age:20;
+}
+
+const myFunction=function(){
+    console.log("Hello bro");
+}
+
+console.log(typeof outsideTemp)
