79dc2ed8c14889ce197aea2869fc69fe7aa3c3da
Avanish Yadav <avanishydv29@gmail.com>
2026-07-25 05:57:55 +0530

    Auto

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,12 +7,17 @@ const repoCount = "4";
 // the below is string interpolation
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Avanishydv')
+const gameName = new String('Avanish-ydv')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
 
-console.log(gameName.length)
-console.log(gameName.toUpperCase());
-console.log(gameName);
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName);
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('i'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
