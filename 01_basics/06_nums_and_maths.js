71e422562560952c1ea751ea21a7856817075295
Avanish Yadav <avanishydv29@gmail.com>
2026-07-26 11:18:17 +0530

    Auto

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,1 +1,10 @@
+//***************/ Lec - 12 ***************//
 const score = 4400
+console.log(score);
+
+const balance = new Number(20090)
+
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
