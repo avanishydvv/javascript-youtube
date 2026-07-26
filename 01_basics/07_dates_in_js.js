1abdbb7e7f0580174b6d5978e2b0624f33df24be
Avanish Yadav <avanishydv29@gmail.com>
2026-07-26 16:27:18 +0530

    Auto

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,10 @@
+/*********** Lec - 13 *******************/
+/*********** Date and Time*******************/
+
+let myDate = new Date()
+console.log(myDate.toString());   /*Sun Jul 26 2026 15:30:44 GMT+0530 (India Standard Time)*/
+console.log(myDate.toISOString()); /*2026-07-26T10:00:44.672Z */
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toTimeString());
+
+
