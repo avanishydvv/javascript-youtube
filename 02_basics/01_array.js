fdac43625f364b2974c67eae28c3110a7b614d3f
Avanish Yadav <avanishydv29@gmail.com>
2026-07-27 21:52:20 +0530

    Auto

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -14,8 +14,28 @@ const myArr2=new Array(1,2,3,4)
 // myArr.push(7)
 // myArr.pop(7)
 
-myArr.unshift(9)
+// myArr.unshift(9)
+// myArr.shift()
 
-console.log(myArr);
+// console.log(myArr.includes(3));
+// console.log(myArr.includes(19));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);   /* this is into string */
+
+// Slice , Splice
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
 
 
+const myn2 = myArr.splice(1,3)
+console.log("C",myn1);
+console.log(myn2);
+
+/*  */
---
+++ 02_basics/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
