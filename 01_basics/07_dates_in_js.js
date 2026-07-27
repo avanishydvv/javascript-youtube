38ac85b8895b25cfc2f2b9ff1acea4ff25270fcf
Avanish Yadav <avanishydv29@gmail.com>
2026-07-27 21:28:25 +0530

    Auto

--- 01_basics/07_dates_in_js.js
+++ 01_basics/07_dates_in_js.js
@@ -15,7 +15,12 @@ let myCreatedDate = new Date("01-14-2026")
 //console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
 
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth());
 
--- 01_basics/tempCodeRunnerFile.js
+++ 01_basics/tempCodeRunnerFile.js
@@ -1,5 +1,2 @@
-console.log(myDate.toString());   /*Sun Jul 26 2026 15:30:44 GMT+0530 (India Standard Time)*/
-// console.log(myDate.toISOString()); /*2026-07-26T10:00:44.672Z */
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toTimeString());
-// console.log(typeof myDate);  // object//
+console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
