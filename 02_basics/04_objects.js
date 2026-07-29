5949cd142b1af76554fbfdb234c0183282fb41f2
Avanish Yadav <avanishydv29@gmail.com>
2026-07-30 04:41:15 +0530

    Auto

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -25,6 +25,36 @@ const obj2 = {1: "a", 4: "b"}
 
 //const obj3 = {obj1,obj2}
 
-const obj3 = Object.assign({},obj1,obj2)
+//const obj3 = Object.assign({},obj1,obj2)
 
-console.log(obj3);
+const obj3 = {...obj1[1],...obj2}     /* Most usable glass tut gaya format */
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 45,
+        email: "hotmail@yahoo.com"
+    },
+    {
+        id: 45,
+        email: "hotmail@yahoo.com"
+    },
+    {
+        id: 45,
+        email: "hotmail@yahoo.com"
+    },
+    {
+        id: 45,
+        workEmail: "hotmail@yahoo.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));             /* Most importtant to extract keys */
+// console.log(Object.values(tinderUser));            
+// console.log(Object.entries(tinderUser));            
+
+console.log(tinderUser.hasOwnproperty('isLoggedIn'));
--- 02_basics/tempCodeRunnerFile.js
+++ 02_basics/tempCodeRunnerFile.js
@@ -1,1 +1,4 @@
-const tinderUser = new objects()
+
+// console.log(Object.keys(tinderUser));             /* Most importtant to extract keys */
+// console.log(Object.values(tinderUser));            
+// console.log(Object.entries(tinderUser));
