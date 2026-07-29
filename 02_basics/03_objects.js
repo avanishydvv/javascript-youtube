940526b299a1059a7390b4b023870c06b9c88b26
Avanish Yadav <avanishydv29@gmail.com>
2026-07-30 04:20:59 +0530

    Auto

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,9 +5,12 @@
 
 // Object Literals
 
+const mySym = Symbol("Key1")
+
 const JsUser = {
     name: "Avanish",
-    "full name": "Avanish Yadav"
+    "full name": "Avanish Yadav",
+    [mySym] : "myKey1",
     age: 19,
     location: "Gorakhpur",
     email: "Avanish@gmail.com",
@@ -15,5 +18,24 @@ const JsUser = {
     lastlogindays: ["Monday","Saturday"]
 }
 
-console.log(JsUser.name);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+// JsUser.email = "avn@gmail.com"
+// Object.freeze(JsUser)
+// JsUser.email = "Hitesh@google.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello Js User");
+    
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js User,${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
--- 02_basics/tempCodeRunnerFile.js
+++ 02_basics/tempCodeRunnerFile.js
@@ -1,1 +1,9 @@
-email
+console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+// JsUser.email = "avn@gmail.com"
+// Object.freeze(JsUser)
+// JsUser.email = "Hitesh@google.com"
+// console.log(JsUser);
