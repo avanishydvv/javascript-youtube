06164abdf16cac3ac81dcb83d9bf243bb137f518
Avanish Yadav <avanishydv29@gmail.com>
2026-07-29 21:18:36 +0530

    Auto

---
+++ 02_basics/02_array.js
@@ -0,0 +1,10 @@
+/********************* Lec 15 ****************/
+// Array
+
+const marvel_heros =["thor","Ironman","Spiderman"]
+const dc_heros =["superman","flash","batman"]
+marvel_heros.push(dc_heros)
+
+console.log(marvel_heros);
+
+
