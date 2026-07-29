83d1e76799e8f64cc304a598d961af6f9d8512a8
Avanish Yadav <avanishydv29@gmail.com>
2026-07-29 22:04:31 +0530

    Auto

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -90,3 +90,25 @@ const JsUser = {
 }
 
 console.log();
+
+
+
+
+
+// ************* lec 16 *********/ 
+/*********** Objects  ***************/
+//Object Create
+//Singleton
+
+// Object Literals
+
+const JsUser = {
+    name: "Avanish",
+    age: 19,
+    location: "Gorakhpur",
+    email: "Avanish@gmail.com",
+    isLoggedIn: false,
+    lastlogindays: ["Monday","Saturday"]
+}
+
+console.log();
