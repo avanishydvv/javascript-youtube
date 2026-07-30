e12d34f271e4940a8e805c5d5a58fdc9908befab
Avanish Yadav <avanishydv29@gmail.com>
2026-07-30 22:08:36 +0530

    Auto

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,15 +1,23 @@
 /****************** lec 21  *********************/
 // Scope
 
-var c = 300
+//var c = 300
+let a = 300
 
 if(true){ 
     let a = 10
     const b = 20
-    var c = 30
+    console.log("Inner",a);             /* Block Scope */
+    
 }
 
-//console.log(a);
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+
+console.log(a);           
 //console.log(b);
-console.log(c);
+//console.log(c);
 
