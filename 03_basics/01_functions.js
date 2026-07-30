dcbb8652dda70b68d81b26ff05daa3a73a32922b
Avanish Yadav <avanishydv29@gmail.com>
2026-07-30 21:58:25 +0530

    Auto

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -56,6 +56,30 @@ function calculateCartPrice(val1,val2,...num1){
     return num1
 }
 
-console.log(calculateCartPrice(200,400,500,2000));
+//console.log(calculateCartPrice(200,400,500,2000));
 
 
+const user={
+    username: "Avanish",
+    price: 999
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username}
+         and price is ${anyobject.price}`);
+    
+}
+handleObject(user)
+handleObject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray =[200,500,800,9000]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,500,800,9000]));
