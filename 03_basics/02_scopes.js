60f5a886f8553868d8d275e3be156a3c3c77bc67
Avanish Yadav <avanishydv29@gmail.com>
2026-08-02 10:46:09 +0530

    Auto

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,26 +2,53 @@
 // Scope
 
 //var c = 300
-let a = 300
+// let a = 300
 
-if(true){ 
-    let a = 10
-    const b = 20
-    console.log("Inner",a);             /* Block Scope */
+// if(true){ 
+//     let a = 10
+//     const b = 20
+//     //console.log("Inner",a);             /* Block Scope */
     
-}
+// }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
 
 
-console.log(a);           
+//console.log(a);           
 //console.log(b);
 //console.log(c);
 
 
 /******************Completed till Lec 21   30/07/2026 ******************************/
+
+/********************* LEC 22 ********************/
+
+function one(){
+    const username = "Avanish"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+    two()
+}
+
+//one()
+
+if(true){
+    const username ="Avanish";
+    if(username="Avanish"){
+        const website = " Youtube"
+        //console.log(username + website);
+        
+    }
+    //console.log(website);
+}
+//console.log(username);
 
--- 03_basics/tempCodeRunnerFile.js
+++ 03_basics/tempCodeRunnerFile.js
@@ -1,1 +1,14 @@
-"Avanish"
+let a = 300
+
+// if(true){ 
+//     let a = 10
+//     const b = 20
+//     //console.log("Inner",a);             /* Block Scope */
+    
+// }
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
