8520ad3fab11bfa828c8313e72eb84ec0841eb04
Avanish Yadav <avanishydv29@gmail.com>
2026-08-04 07:20:49 +0530

    Auto

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,23 @@
+/******************** lec 26 **********/
+/************* Switch case statement **********/
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+
+switch (month) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
