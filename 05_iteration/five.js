fb558d5a49c3c2c22f4d80fbfbf6174d7216f631
Avanish Yadav <avanishydv29@gmail.com>
2026-08-05 23:27:30 +0530

    Auto

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -1,3 +1,46 @@
 // LEC 29
 
 const coding = ["js","rb","py","java","cpp"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+    
+// } )
+
+// coding.forEach( (item)=> {
+//     console.log(item);
+    
+// } )
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item,index,arr) => {
+//     console.log(item,index,arr);
+    
+// })
+
+const myCoding = [
+    {
+    languageName: "python",
+    languageFileName: "py"
+    },
+    {
+    languageName: "java",
+    languageFileName: "java"
+    }
+]
+
+myCoding.forEach( (item)=>{
+    console.log(item.languageName);
+    
+})
+
+
+/****************** LEC 29 COmpleted **********/
+
+/********************** 05/08/2026 **************/
