c4f5fd5363f1cfbfcf4967d283e15f2f9594a1fe
Avanish Yadav <avanishydv29@gmail.com>
2026-08-05 22:22:43 +0530

    Auto

---
+++ 05_iteration/two.js
@@ -0,0 +1,31 @@
+/*********************** LEC 28  **************/
+
+// While Loop  and Do while loop
+
+// let index = 0
+
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index += 2
+// }
+
+//let myArray = ['spiderman' , 'batman' , 'ironman']
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr +=1
+// }
+
+// do while loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score ++
+    
+} while (score <= 10);
+
+
+/******************** Lec 28 is completed  ************/
