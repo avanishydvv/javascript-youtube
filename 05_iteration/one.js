1f23c588b58af33aa128cba32f442b483654a62a
Avanish Yadav <avanishydv29@gmail.com>
2026-08-05 22:04:48 +0530

    Auto

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -15,15 +15,55 @@
 
 //console.log(element);
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`outer loop ${i}`);
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`outer loop ${i}`);
     
-    for (let j = 0; j < 10; j++) {
+//     for (let j = 0; j < 10; j++) {
         
-        //console.log(`Inner loop ${j} and inner loop ${i}`);
-        console.log(i + '*' + j +  '=' + i*j);
+//         //console.log(`Inner loop ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j +  '=' + i*j);
         
+        
+//     }
+    
+// }
+
+
+// let myArray = ["Flash","Batman","pawan Singh"]
+
+// console.log(myArray.length);
+
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+
+// KeyWords 
+// break and continue
+
+// for (let index = 0; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break;
+        
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
+
+for (let index = 0; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue;
         
     }
+    console.log(`value of i is ${index}`);
     
 }
+
+/******************* lec 27 completed  ****************/
+
+//   05/08/2026 
