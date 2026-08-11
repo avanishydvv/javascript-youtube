0aa1d6044c642a68dbf6b34179d7be2d3f48df9c
Avanish Yadav <avanishydv29@gmail.com>
2026-08-11 19:59:36 +0530

    Auto

--- 05_iteration/eight.js
+++ 05_iteration/eight.js
@@ -2,3 +2,39 @@
 
 // reduce method used in shoppping
 
+
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function (acc,currVal) {
+// console.log(`acc:${acc} and cuuVal:${currVal}`);
+    
+//     return acc + currVal ;
+// },0)
+
+const myTotal = myNums.reduce( (acc,currVal) => acc+currVal, 0 )
+
+
+console.log(myTotal);
+
+
+const shoppingCart =[
+    {
+        itemName : "js Course",
+        price:  3999
+    },
+
+    {
+        itemName : "Mobile dev Course",
+        price:  9999
+    },
+
+    {
+        itemName : "Data Science Course",
+        price:  24999
+    }
+]
+
+const priceToPay = shoppingCart.reduce( (acc,item) => acc + item.price, 0)
+
+
+console.log(priceToPay);
