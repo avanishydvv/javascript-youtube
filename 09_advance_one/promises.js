81826e0d0e30f63b009663ecbf34b286b1dc1b4f
Avanish Yadav <avanishydv29@gmail.com>
2026-09-05 08:32:08 +0530

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,3 +1,5 @@
+const { useActionState } = require("react");
+
 /*************** Lec 40 Promises *******************/
 const promiseOne = new Promise(function(resolve,reject){
     // Do an async task
@@ -12,3 +14,55 @@ promiseOne.then(function(){
     console.log("Promise consumed");
     
 })
+
+// 2nd method 
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+        
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+    
+})
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+    resolve({username:"avanish",
+            email:"avn@gmail.com"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+    
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true;
+        if (!error) {
+            resolve({username:"hitesh",password:"12345"})
+            
+        }
+        else{
+            reject('ERROR : Something went wrong')
+        }
+
+    },2000)
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username 
+    
+}).then((username) => {
+    console.log(username);
+    
+}).catch(function(error){
+    console.log(error);
+})
+
+
