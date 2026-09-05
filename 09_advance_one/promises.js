10ebe59511ba5488e9e2a161602c6869c36fc5d7
Avanish Yadav <avanishydv29@gmail.com>
2026-09-05 08:15:27 +0530

    Update

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,2 +1,14 @@
 /*************** Lec 40 Promises *******************/
-const promiseOne = new Promise()
+const promiseOne = new Promise(function(resolve,reject){
+    // Do an async task
+    // db calls,cryptography, network call
+    setTimeout(function(){
+        console.log('Async task is completed');
+        resolve()
+    },1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+    
+})
